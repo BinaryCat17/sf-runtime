4b39a447c75b9491e397148d8d65bef0d4b5b885
BinaryCat17 <vv.smirnov17@ya.ru>
2026-01-06 15:50:40 +0300

    runtime: implement specification-first CPU kernel generation

--- ops/src/sf_ops_math.c
+++ ops/src/sf_ops_math.c
@@ -8,20 +8,8 @@
 
 /**
  * SionFlow Atomic Kernels
- * Automatically generated from sf_ops_db.inc
+ * Note: Automatic kernels are now in sf_ops_auto.c
  */
-
-#define SF_GEN_AUTO(_op, _ke, _ar) SF_KERNEL_AUTO(_op, _ke, _ar)
-#define SF_GEN_MANUAL(...)
-
-#define SF_OP(_s, _n, _op, _cat, _strat, _in, _out, _tr, _sr, _ar, _p1, _p2, _p3, _p4, _kt, _ke, _arity) \
-    SF_GEN_##_kt(_op, _ke, _arity)
-
-SF_OP_LIST
-
-#undef SF_OP
-#undef SF_GEN_AUTO
-#undef SF_GEN_MANUAL
 
 // --- Vector Math (Custom Kernels) ---
 
