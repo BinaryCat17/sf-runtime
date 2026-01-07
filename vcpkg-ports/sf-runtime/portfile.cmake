4c517b2f308ebcb64a7ee7364e2ad5deba8eaeb8
BinaryCat17 <smirnov.vladimir17@gmail.com>
2026-01-07 10:25:24 +0300

    fix

--- vcpkg-ports/sf-runtime/portfile.cmake
+++ vcpkg-ports/sf-runtime/portfile.cmake
@@ -8,8 +8,6 @@ vcpkg_cmake_configure(
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME sf-runtime CONFIG_PATH lib/cmake/sf-runtime)
 
-vcpkg_copy_tools(TOOL_NAMES sf-runner sf-window AUTO_CLEAN)
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 if(EXISTS "${SOURCE_PATH}/LICENSE")
